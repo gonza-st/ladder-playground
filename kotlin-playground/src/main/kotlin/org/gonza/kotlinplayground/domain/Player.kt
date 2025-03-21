f269abfc4e87b7fa92b2bd06681dcfabb2bc0104
Choi_Kyle <az86067@gmail.com>
2025-03-21 13:22:23 +0900

    feat: add player test

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/Player.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/Player.kt
@@ -4,5 +4,17 @@ data class Player(
     val name: String,
     val position: Point
 ) {
+    companion object {
+        private const val MIN_NAME_LENGTH = 1
+        private const val MAX_NAME_LENGTH = 5
+    }
+    init {
+        require(name.length <= MAX_NAME_LENGTH) {
+            throw IllegalArgumentException("이름 길이가 5자 이하여야 합니다.")
+        }
 
+        require(name.length >= MIN_NAME_LENGTH) {
+            throw IllegalArgumentException("이름 길이가 1글자 이상이어야 합니다.")
+        }
+    }
 }
--- kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/domain/PlayerTest.kt
+++ kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/domain/PlayerTest.kt
@@ -46,8 +46,40 @@ class PlayerTest {
     fun `참가자의 이름은 5글자 초과면 에러가 발생한다`(name: String) {
         val position = Point(x = 0, y = 0)
 
-//        Assertions.assertThatThrownBy { Player(name = name, position = position) }
-//            .isInstanceOf(IllegalArgumentException::class.java)
-//            .hasMessage("올바른 값이 아닙니다")
+        Assertions.assertThatThrownBy { Player(name = name, position = position) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("이름 길이가 5자 이하여야 합니다.")
+    }
+
+    @Test
+    fun `참가자의 이름은 1글자 미만이면 에러가 발생한다`() {
+        val emptyName = ""
+        val position = Point(x = 0, y = 0)
+
+        Assertions.assertThatThrownBy { Player(name = emptyName, position = position) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("이름 길이가 1글자 이상이어야 합니다.")
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            " trim ",
+            " trim",
+            "trim ",
+        ],
+        ignoreLeadingAndTrailingWhiteSpace = false
+    )
+    fun `참가자의 이름의 양 옆엔 공백이 있으면, 제거하고 검증한다`(name: String) {
+        val position = Point(x = 0, y = 0)
+
+        val trimmedName = "trim"
+
+        val player = Player(name = name, position = position)
+        val player2 = Player(name = trimmedName, position = position)
+
+        println("player = '${player.name}'")
+        println("player2 = '${player2.name}'")
+        assertThat(player).isEqualTo(player2)
     }
 }
