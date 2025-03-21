86b49e7590ea63c6e039474f963dad7ce5ea88f3
leeMK09 <dlaudrb09@naver.com>
2025-03-21 13:30:26 +0900

    test: 플레이어 이름에 대한 검증 추가

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/PlayersCreator.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/PlayersCreator.kt
@@ -3,7 +3,24 @@ package org.gonza.kotlinplayground.service
 import org.gonza.kotlinplayground.domain.Players
 
 class PlayersCreator {
-    fun create(allPlayerName: String): Players = Players(emptyList())
+    fun create(allPlayerName: String): Players {
+        val playerNameList = splitPlayerName(allPlayerName)
+
+        validatePlayerNameList(playerNameList)
+
+        return Players(emptyList())
+    }
+
+    private fun validatePlayerNameList(splitPlayerList: List<String>) {
+        val isValid =
+            splitPlayerList.none {
+                it.isBlank()
+            }
+
+        check(isValid) {
+            throw IllegalArgumentException("플레이어가 없습니다.")
+        }
+    }
 
     private fun splitPlayerName(allPlayerName: String): List<String> = allPlayerName.split(",")
 }
