eab68c394227e7287a5d88499e9a7b920a721b14
Choi_Kyle <az86067@gmail.com>
2025-03-21 12:53:51 +0900

    feat: add player name test

--- kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/domain/PlayerTest.kt
+++ kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/domain/PlayerTest.kt
@@ -1,7 +1,10 @@
 package org.gonza.kotlinplayground.domain
 
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class PlayerTest {
 
@@ -15,5 +18,36 @@ class PlayerTest {
         assertThat(player).isNotNull
         assertThat(player).hasFieldOrProperty("name")
         assertThat(player).hasFieldOrProperty("position")
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "aaaaa",
+        "aaaa",
+        "aaa",
+        "aa",
+        "a"
+    )
+    fun `참가자의 이름은 5글자 이하이다`(name: String) {
+        val position = Point(x = 0, y = 0)
+
+        val player = Player(name = name, position = position)
+
+        assertThat(player).isNotNull
+        assertThat(player).hasFieldOrProperty("name")
+        assertThat(player).hasFieldOrProperty("position")
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "aaaaaa",
+        "aaaaaaaa"
+    )
+    fun `참가자의 이름은 5글자 초과면 에러가 발생한다`(name: String) {
+        val position = Point(x = 0, y = 0)
+
+//        Assertions.assertThatThrownBy { Player(name = name, position = position) }
+//            .isInstanceOf(IllegalArgumentException::class.java)
+//            .hasMessage("올바른 값이 아닙니다")
     }
 }
