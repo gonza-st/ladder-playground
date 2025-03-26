ab997a06905923494db40bcae7207fc5beefc9a6
currenjin <hyun0524e@naver.com>
2025-03-26 13:46:28 +0900

    Feat: Printer, Reader, Screen

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/screen/Screen.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/screen/Screen.kt
@@ -1,4 +1,13 @@
 package org.gonza.kotlinplayground.screen
 
-interface Screen {
+import org.gonza.kotlinplayground.ladder.LadderCreator
+import org.gonza.kotlinplayground.ladder.LadderCreatorImpl
+import org.gonza.kotlinplayground.player.LadderGamePlayerCreator
+import org.gonza.kotlinplayground.player.PlayerCreator
+
+class Screen {
+    val ladderCreator: LadderCreator = LadderCreatorImpl()
+    val playerCreator: PlayerCreator = LadderGamePlayerCreator()
+
+    // TODO: Logic
 }
